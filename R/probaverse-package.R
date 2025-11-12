b70a823509c2d0120026cc2d7a557fc1f3ade196
Vincenzo Coia <vincenzo.coia@gmail.com>
2025-11-12 23:33:38 +0100

    Solve 'Namespace in Imports field not imported from' note in check().

--- R/probaverse-package.R
+++ R/probaverse-package.R
@@ -4,3 +4,14 @@
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
+
+# From tidyverse:
+# Suppress R CMD check note
+# Namespace in Imports field not imported from: PKG
+#   All declared Imports should be used.
+ignore_unused_imports <- function() {
+  distionary::distribution
+  distplyr::mix
+  famish::fit_dst
+}
