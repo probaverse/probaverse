bee11eba9592b5e0cd481bc999cfff8ab4bca499
Vincenzo Coia <vincenzo.coia@gmail.com>
2025-11-13 11:04:40 +0100

    update docs

--- tests/testthat/test-package-loading.R
+++ tests/testthat/test-package-loading.R
@@ -3,3 +3,7 @@ test_that("All core packages load correctly", {
   expect_true(requireNamespace("distplyr", quietly = TRUE))
   expect_true(requireNamespace("famish", quietly = TRUE))
 })
+
+test_that("ignore_unused_imports() trick doesn't error.", {
+  expect_no_error(ignore_unused_imports())
+})
